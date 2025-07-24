6b0bd5a22c2f37a69113003d8d2437af864c7712
vncnttan <vincent.tanjaya.vt@gmail.com>
2025-07-24 19:17:36 +0700

    feat: Fix Header Section 📊

--- src/components/ui/leaderboard/LeaderboardHeaderSection.tsx
+++ src/components/ui/leaderboard/LeaderboardHeaderSection.tsx
@@ -1,11 +1,19 @@
 import { LeaderboardItems } from "@/components/ui/leaderboard/LeaderboardItem.tsx";
+import { Slider } from "@/components/ui/slider.tsx";
+import { cn } from "@/lib/utils.ts";
 
 export default function LeaderboardHeaderSection() {
   return (
     <LeaderboardItems
       endSection={
-        <div className="space-y-0.5 place-items-end">
+        <div className="space-y-2 place-items-end">
           <div className="font-medium">Next Level Progress</div>
+          <Slider
+            defaultValue={[50]}
+            max={100}
+            step={1}
+            className={cn("w-[150%]")}
+          />
           <div className="font-medium">110 XP to level 14</div>
         </div>
       }
