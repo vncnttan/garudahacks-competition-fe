eb36082b6df523c3040d4344146d7c3a11d6ba56
vncnttan <vincent.tanjaya.vt@gmail.com>
2025-07-24 11:58:51 +0700

    feat: Poppins font and navbar 🎨

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,13 +1,14 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Landing from "@/pages/Landing.tsx";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from "@/layout/MainLayout.tsx";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<div>Home Page</div>} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
---
+++ src/components/common/app-navbar.tsx
@@ -0,0 +1,15 @@
+export default function AppNavbar() {
+  return (
+    <nav className="bg-black text-white p-4">
+      <div className="container mx-auto flex justify-between place-items-center">
+        <h1 className="text-white text-2xl">Logo</h1>
+        <div className="flex flex-row gap-24">
+          <div>Leaderboards</div>
+          <div>Dictionary</div>
+          <div>Video Chat</div>
+        </div>
+        <div className="text-white text-base">Vincent Tanjaya</div>
+      </div>
+    </nav>
+  );
+}
