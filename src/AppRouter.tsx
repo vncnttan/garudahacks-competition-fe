3d56bbeb2ba3a2b2e26e508f7fff577feb0cd827
vncnttan <vincent.tanjaya.vt@gmail.com>
2025-07-24 23:33:02 +0700

    feat: Wordle buggy 📊

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -5,6 +5,7 @@ import Dictionary from "./pages/Dictionary";
 import AddNewWord from "./pages/AddNewWord";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Wordle from "@/components/common/wordle/wordle.tsx";
 
 export default function AppRouter() {
   return (
@@ -14,6 +15,7 @@ export default function AppRouter() {
           <Route path="/" element={<Dictionary />} />
           <Route path="/leaderboard" element={<Leaderboard />} />
           <Route path="/add-new-word" element={<AddNewWord />} />
+          <Route path="/wordle" element={<Wordle />} />
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,64 +1,77 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
 
 export default function Register() {
-    const [username, setUsername] = useState<string>();
-    const [password, setPassword] = useState<string>();
-    const [confirmPassword, setConfirmPassword] = useState<string>();
+  const [username, setUsername] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const [confirmPassword, setConfirmPassword] = useState<string>();
 
-    return (
-        <div className="w-screen h-screen bg-black flex items-center justify-center">
-            <Card className="w-full max-w-md">
-                <CardHeader>
-                    <CardTitle className="text-2xl">Register</CardTitle>
-                    <CardDescription>Enter your details to create an account.</CardDescription>
-                </CardHeader>
+  return (
+    <div className="w-screen h-screen bg-black flex items-center justify-center">
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle className="text-2xl">Register</CardTitle>
+          <CardDescription>
+            Enter your details to create an account.
+          </CardDescription>
+        </CardHeader>
 
-                <CardContent>
-                    <div className="flex flex-col gap-5">
-                        <div className="flex flex-col gap-2">
-                            <Label htmlFor="username">Username *</Label>
-                            <Input
-                                className="w-full"
-                                id="username"
-                                placeholder="Username"
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}
-                            />
-                        </div>
+        <CardContent>
+          <div className="flex flex-col gap-5">
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="username">Username *</Label>
+              <Input
+                className="w-full"
+                id="username"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
 
-                        <div className="flex flex-col gap-2">
-                            <Label htmlFor="password">Password *</Label>
-                            <Input
-                                className="w-full"
-                                type="password"
-                                id="password"
-                                placeholder="Password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                        </div>
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="password">Password *</Label>
+              <Input
+                className="w-full"
+                type="password"
+                id="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
 
-                        <div className="flex flex-col gap-2">
-                            <Label htmlFor="confirmPassword">Confirm Password *</Label>
-                            <Input
-                                className="w-full"
-                                type="password"
-                                id="confirmPassword"
-                                placeholder="Confirm Password"
-                                value={confirmPassword}
-                                onChange={(e) => setConfirmPassword(e.target.value)}
-                            />
-                        </div>
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="confirmPassword">Confirm Password *</Label>
+              <Input
+                className="w-full"
+                type="password"
+                id="confirmPassword"
+                placeholder="Confirm Password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
 
-                        <Button onClick={}>Register</Button>
-                    </div>
-
-                </CardContent>
-            </Card>
-        </div>
-    )
+            <Button
+              onClick={() => {
+                console.log("Register clicked");
+              }}
+            >
+              Register
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
